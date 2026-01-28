9b6706b98fa71e2f605b33e0312d9c7467455f73
KevinCNCC <kevin.crook@student.northampton.edu>
2026-01-28 18:55:11 +0000

    bmi assignment

--- main.cpp
+++ main.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 
 int main() {
- 
-
-
+    int height = 61;
+    int weight = 130;
+    double kg, meters;
+    kg = weight*0.453592;
+    meters = height*0.0254;
+    double bmi;
+    bmi = kg/(meters*meters);
+    cout << "The BMI for this person is: " << bmi << endl;
 }
